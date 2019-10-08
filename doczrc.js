d0b9486cea3011e02fd57a4610a463168dc27ea2
Mateusz Kocz <mateusz@kocz.pl>
2019-10-08 11:02:46 +0200

    Create project and add assessment section

---
+++ doczrc.js
@@ -0,0 +1,10 @@
+export default {
+  src: "./docs/",
+  menu: [
+    "App'n'roll Developers",
+    {
+      name: "Assessment",
+      menu: ["Introduction", "Developers’ Levels", "Developers’ Skills"]
+    }
+  ]
+}
