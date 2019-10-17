38b8752faae6af2f9a164b7e9240ee1f80c096fc
Mateusz Kocz <mateusz@kocz.pl>
2019-10-17 11:57:05 +0200

    release #4

--- styles-50b9797b805402081703.js
+++ styles-50b9797b805402081703.js
@@ -1,2 +1,2 @@
 (window.webpackJsonp=window.webpackJsonp||[]).push([[7],{372:function(n,w,o){}}]);
-//# sourceMappingURL=styles-ddbfc6e24da5556eb262.js.map
+//# sourceMappingURL=styles-50b9797b805402081703.js.map
