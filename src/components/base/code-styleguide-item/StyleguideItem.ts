d66fa2575375a318c7100f2c3b5da467a7791523
Mateusz Kocz <mateusz@kocz.pl>
2020-02-02 19:31:46 +0100

    Add styleguides and some placeholder content

---
+++ src/components/base/code-styleguide-item/StyleguideItem.ts
@@ -0,0 +1,7 @@
+export default interface StyleguideItem {
+  id: string
+  frontmatter: {
+    title: string
+  }
+  body: string
+}
--- src/pages/styleguide.tsx
+++ src/pages/styleguide.tsx
@@ -1,14 +1,48 @@
+import { graphql } from "gatsby"
 import React from "react"
+import CodeStyleguideItem from "../components/base/code-styleguide-item/code-styleguide-item"
+import StyleguideItem from "../components/base/code-styleguide-item/StyleguideItem"
+import LayoutBlock from "../components/base/layout-block/layout-block.component"
 import PageTitle from "../components/base/page-title/page-title.component"
 import Layout from "../components/layout"
 import AppFunctionComponent from "../types/app-function-component.interface"
 
-const StyleGuidePage: AppFunctionComponent = () => {
+interface Props {
+  data: {
+    guides: {
+      nodes: StyleguideItem[]
+    }
+  }
+}
+
+const StyleGuidePage: AppFunctionComponent<Props> = ({
+  data: {
+    guides: { nodes: guides },
+  },
+}) => {
   return (
     <Layout>
       <PageTitle>Code Styleguide</PageTitle>
+      <LayoutBlock>
+        {guides.map(({ id, body, frontmatter: { title } }) => (
+          <CodeStyleguideItem title={title} key={id} body={body} />
+        ))}
+      </LayoutBlock>
     </Layout>
   )
 }
 
 export default StyleGuidePage
+
+export const query = graphql`
+  {
+    guides: allMdx(filter: { fileAbsolutePath: { glob: "**/styleguide/**" } }) {
+      nodes {
+        frontmatter {
+          title
+        }
+        body
+      }
+    }
+  }
+`
