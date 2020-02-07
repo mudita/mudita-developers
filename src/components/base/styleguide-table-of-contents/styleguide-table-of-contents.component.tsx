afcfed4a814e51ebdf852b5855ad52604516d0ca
Mateusz Kocz <mateusz@kocz.pl>
2020-02-07 15:46:43 +0100

    Display a nested structure in the styleguide's toc

--- src/components/base/styleguide-table-of-contents/styleguide-table-of-contents.component.tsx
+++ src/components/base/styleguide-table-of-contents/styleguide-table-of-contents.component.tsx
@@ -1,17 +1,35 @@
 import React from "react"
 import AppFunctionComponent from "../../../types/app-function-component.interface"
+import StyleguideTableOfContentsItem from "./styleguide-table-of-contents-item.interface"
 
 interface Props {
-  items: { url: string; title: string }[]
+  items: StyleguideTableOfContentsItem[]
+}
+
+const Item: AppFunctionComponent<StyleguideTableOfContentsItem> = ({
+  url,
+  title,
+  items,
+}) => {
+  return (
+    <li>
+      <a href={url}>{title}</a>
+      {items && (
+        <ul>
+          {items.map(item => (
+            <Item key={item.url} {...item} />
+          ))}
+        </ul>
+      )}
+    </li>
+  )
 }
 
 const StyleguideTableOfContents: AppFunctionComponent<Props> = ({ items }) => {
   return (
     <ul>
-      {items.map(({ title, url }) => (
-        <li key={url}>
-          <a href={url}>{title}</a>
-        </li>
+      {items.map(item => (
+        <Item key={item.url} {...item} />
       ))}
     </ul>
   )
--- src/pages/styleguide.tsx
+++ src/pages/styleguide.tsx
@@ -3,6 +3,7 @@ import React from "react"
 import CodeStyleGuide from "../components/base/code-style-guide/code-style-guide.component"
 import LayoutBlock from "../components/base/layout-block/layout-block.component"
 import PageTitle from "../components/base/page-title/page-title.component"
+import StyleguideTableOfContentsItem from "../components/base/styleguide-table-of-contents/styleguide-table-of-contents-item.interface"
 import StyleguideTableOfContents from "../components/base/styleguide-table-of-contents/styleguide-table-of-contents.component"
 import Layout from "../components/layout"
 import AppFunctionComponent from "../types/app-function-component.interface"
@@ -12,7 +13,7 @@ interface Props {
     guide: {
       body: string
       tableOfContents: {
-        items: { url: string; title: string }[]
+        items: StyleguideTableOfContentsItem[]
       }
     }
   }
