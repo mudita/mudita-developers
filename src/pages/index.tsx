e0aea66cc5b8be8bf4db69f58e1af08499c3194d
Mateusz Kocz <mateusz@kocz.pl>
2020-04-25 18:28:28 +0200

    Remove placeholder content from th ehomee page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,5 @@
 import React from "react"
-import Feature from "../components/base/feature/feature.component"
 import Hero from "../components/base/hero/hero.component"
-import GuideLayout from "../components/base/layout-block/guide-layout"
 import RecruitmentAd from "../components/base/recruitment-ad/recruitment-ad.component"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -12,76 +10,6 @@ const IndexPage: AppFunctionComponent = () => (
     <SEO title="App'n'roll Developers" />
     <Hero />
     <RecruitmentAd />
-    <GuideLayout>
-      <Feature
-        title="We're blogging"
-        linkText="Read the blog"
-        linkPath="/blog"
-        textSide="left"
-      >
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur
-          corporis culpa delectus doloremque, esse eum fugit illo in iste iure
-          laborum nemo nisi non quaerat quia quos recusandae soluta veniam!
-        </p>
-
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur
-          corporis culpa delectus doloremque, esse eum fugit illo in iste iure
-          laborum nemo nisi non quaerat quia quos recusandae soluta veniam!
-        </p>
-      </Feature>
-      <Feature
-        title="Meet the Devs"
-        linkText="Developers"
-        linkPath="/developers"
-        textSide="right"
-      >
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur
-        corporis culpa delectus doloremque, esse eum fugit illo in iste iure
-        laborum nemo nisi non quaerat quia quos recusandae soluta veniam!
-      </Feature>
-      <Feature
-        title="Our tech"
-        linkText="Learn the technologies"
-        linkPath="/technologies"
-        textSide="left"
-      >
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur
-        corporis culpa delectus doloremque, esse eum fugit illo in iste iure
-        laborum nemo nisi non quaerat quia quos recusandae soluta veniam!
-      </Feature>
-      <Feature
-        title="Code style"
-        linkText="See how we code"
-        linkPath="/code-style"
-        textSide="right"
-      >
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur
-        corporis culpa delectus doloremque, esse eum fugit illo in iste iure
-        laborum nemo nisi non quaerat quia quos recusandae soluta veniam!
-      </Feature>
-      <Feature
-        title="Developer Guide"
-        linkText="Read the guide"
-        linkPath="/guide"
-        textSide="left"
-      >
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur
-        corporis culpa delectus doloremque, esse eum fugit illo in iste iure
-        laborum nemo nisi non quaerat quia quos recusandae soluta veniam!
-      </Feature>
-      <Feature
-        title="Useful resources"
-        linkText="Browse the resources"
-        linkPath="/resources"
-        textSide="right"
-      >
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur
-        corporis culpa delectus doloremque, esse eum fugit illo in iste iure
-        laborum nemo nisi non quaerat quia quos recusandae soluta veniam!
-      </Feature>
-    </GuideLayout>
   </Layout>
 )
 
