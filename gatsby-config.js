129cfe1f95d021c038961c2ede3d1756a763f400
Mateusz Kocz <mateusz@kocz.pl>
2020-04-25 15:18:33 +0200

    Remove broken fonts plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -100,13 +100,6 @@ module.exports = {
     },
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-typescript`,
-    {
-      resolve: `gatsby-plugin-google-fonts`,
-      options: {
-        fonts: [`ubuntu`],
-        display: "swap",
-      },
-    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
