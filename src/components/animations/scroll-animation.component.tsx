5680d5077ac43f3a9e9eb73ccc947311f0b65afd
Kamil Unai <kamilxunai@yahoo.com>
2020-04-28 18:15:20 +0200

    add style

--- src/components/animations/scroll-animation.component.tsx
+++ src/components/animations/scroll-animation.component.tsx
@@ -1,19 +1,46 @@
 import React from "react"
 import AppFunctionComponent from "../../types/app-function-component.interface"
+import styled from "styled-components"
+
+const Root = styled.section`
+  background-color: #fbab7e;
+  background-image: linear-gradient(62deg, #fbab7e 0%, #f7ce68 100%);
+`
+
+const Top = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 20rem;
+`
+
+const Box = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 5rem;
+  padding: 5rem;
+  width: 60%;
+  box-shadow: 10px 10px 40px 10px rgba(0, 0, 0, 0.4);
+  margin: 0 auto 5rem;
+  background-color: #f4d03f;
+  background-image: linear-gradient(132deg, #f4d03f 0%, #16a085 100%);
+`
 
 const ScrollAnimation: AppFunctionComponent = () => {
   return (
-    <section>
-      <div>Scroll ↓</div>
-      <div>Kamil P</div>
-      <div>Kamil S</div>
-      <div>Mateusz</div>
-      <div>Kasia</div>
-      <div>Sylwia</div>
-      <div>Ula</div>
-      <div>Michał</div>
-      <div>Igor</div>
-    </section>
+    <Root>
+      <Top>Scroll ↓</Top>
+      <Box>Kamil P</Box>
+      <Box>Kamil S</Box>
+      <Box>Mateusz</Box>
+      <Box>Kasia</Box>
+      <Box>Sylwia</Box>
+      <Box>Ula</Box>
+      <Box>Michał</Box>
+      <Box>Igor</Box>
+    </Root>
   )
 }
 
