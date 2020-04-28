ff84fcc339fdae077a9dc4efa57716760eea7110
Kamil Unai <kamilxunai@yahoo.com>
2020-04-28 17:34:57 +0200

    add people

--- src/components/animations/scroll-animation.component.tsx
+++ src/components/animations/scroll-animation.component.tsx
@@ -2,7 +2,19 @@ import React from "react"
 import AppFunctionComponent from "../../types/app-function-component.interface"
 
 const ScrollAnimation: AppFunctionComponent = () => {
-  return <div>Scroll Animation</div>
+  return (
+    <section>
+      <div>Scroll ↓</div>
+      <div>Kamil P</div>
+      <div>Kamil S</div>
+      <div>Mateusz</div>
+      <div>Kasia</div>
+      <div>Sylwia</div>
+      <div>Ula</div>
+      <div>Michał</div>
+      <div>Igor</div>
+    </section>
+  )
 }
 
 export default ScrollAnimation
