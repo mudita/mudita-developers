2094af1a7903dbf503f2b9e6a69de39423ccdf92
Sylwia Szymańska <sylwiakszymanska@gmail.com>
2020-04-28 18:34:47 +0200

    Add menu and hero section components

--- src/components/animations/hero-section.component.tsx
+++ src/components/animations/hero-section.component.tsx
@@ -1,8 +1,42 @@
 import React from "react"
 import AppFunctionComponent from "../../types/app-function-component.interface"
+import styled from "styled-components"
+import snowImage from "../../images/snow.jpg"
+
+const Parallax = styled.div`
+  min-height: 200vh;
+`
+const TextWithBackground = styled.p`
+  font-weight: 1000;
+  -webkit-background-clip: text;
+  color: transparent;
+  background-image: url(${snowImage});
+  font-size: 360px;
+  line-height: 360px;
+  margin-left: 50px;
+  word-break: break-all;
+  background-clip: text;
+  background-size: cover;
+  background-position-x: 542px;
+  background-position-y: 951px;
+`
+
+// const getViewportHeight = () => {
+//   const height = document.documentElement.clientHeight
+//   const innerHeight = window.innerHeight;
+//   return height < innerHeight ? innerHeight : height;
+// }
+//
+// const animate = () => {
+//   console.log("animate")
+// }
 
 const HeroSection: AppFunctionComponent = () => {
-  return <div>Hero Section</div>
+  return (
+    <Parallax>
+      <TextWithBackground>Parallax effect!</TextWithBackground>
+    </Parallax>
+  )
 }
 
 export default HeroSection
--- src/components/animations/side-menu.component.tsx
+++ src/components/animations/side-menu.component.tsx
@@ -1,8 +1,71 @@
 import React from "react"
 import AppFunctionComponent from "../../types/app-function-component.interface"
+import PictureIcon from "../../images/picture.svg"
+import StarIcon from "../../images/star.svg"
+import SettingsIcon from "../../images/settings.svg"
+import StopwatchIcon from "../../images/picture.svg"
+import styled from "styled-components"
+
+const Menu = styled.div`
+  position: fixed;
+  left: 0;
+  top: 0;
+  height: 100vh;
+  width: 50px;
+  display: flex;
+  justify-content: space-around;
+  flex-direction: column;
+  background-color: #0a090e;
+
+  &:hover {
+    svg {
+      fill: red;
+      width: 40px;
+      height: 40px;
+    }
+  }
+`
+const MenuElement = styled.li`
+  display: inline-block;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  svg {
+    width: 30px;
+    height: 30px;
+    fill: #fff;
+    transition: width 2s, height 2s, fill 1s;
+  }
+`
+
+const menuElements = [
+  {
+    icon: <PictureIcon />,
+    name: "picture",
+  },
+  {
+    icon: <StarIcon />,
+    name: "star",
+  },
+  {
+    icon: <SettingsIcon />,
+    name: "settings",
+  },
+  {
+    icon: <StopwatchIcon />,
+    name: "stopwatch",
+  },
+]
 
 const SideMenu: AppFunctionComponent = () => {
-  return <div>Side Menu</div>
+  return (
+    <Menu>
+      {menuElements.map(item => (
+        <MenuElement key={item.name}>{item.icon}</MenuElement>
+      ))}
+    </Menu>
+  )
 }
 
 export default SideMenu
