8cab06a4b65b7294e20a8771d5e69f4dacff5b9c
ulaulala <ukazmierczyk@gmail.com>
2020-05-11 06:42:17 +0200

    Fix react-helmet import

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -7,7 +7,7 @@
 
 import { graphql, useStaticQuery } from "gatsby"
 import React from "react"
-import Helmet from "react-helmet"
+import { Helmet } from "react-helmet"
 
 type MetaProps = JSX.IntrinsicElements["meta"]
 
